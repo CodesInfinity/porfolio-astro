f1fc182e23f1f293b09983d496c6aaedc30ff0e1
Agustín Rodríguez <64686920+CodesInfinity@users.noreply.github.com>
2024-07-24 12:19:05 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "",
+  site: "https://agustinrodriguez.netlify.app",
   integrations: [
     tailwind(),
     sitemap({

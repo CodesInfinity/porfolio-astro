83cf2a97c49ac2af59f1146e18b59fb0d8a33ac9
CodesInfinity <agusmatic21@gmail.com>
2024-07-24 12:07:21 +0200

    Subida

---
+++ src/data/info.ts
@@ -0,0 +1,66 @@
+export const info = {
+  baseUrl: "",
+  name: "Agustín Rodríguez",
+  jobDescription: "Web Developer",
+  about: `Un estudiante de Ingeniería de Software en la Universidad de Huelva. 
+  Me especializo en desarrollo web gracias a mis estudios y formación académica. Aunque aún no tengo mucha
+  experiencia profesional en el campo, estoy comprometido con mi aprendizaje continuo, explorando 
+  nuevas oportunidades para aplicar mis conocimientos y crecer en esta área.
+  `,
+
+  experience: [
+    {
+      name: "Herbecon Systems",
+      location: "Sierra de yeguas, Málaga",
+      startDate: "Abr 2022",
+      endDate: "Jun 2022",
+      description: [
+        "Desarrollador web encargado de mejorar funcionalidades, desarrollo de componentes y actualización de FAQ",
+        "Stack: Typescript · Angular · Ionic · MongoDB · Git.",
+      ],
+    }
+  ],
+
+  education: [
+    {
+      name: "Grado en Ingeniería Informática, especialidad de Software",
+      location: "Universidad de Huelva, Huelva",
+      startDate: "2022",
+      endDate: "Actualidad",
+      description: [
+        "Aptitudes: Java · VHDL · C++ · C · SQL · MATLAB · Estructuras de datos · Gestión de proyectos · Metodologías Ágiles",
+      ],
+    },
+    {
+      name: "CFGS Desarrollo de Aplicaciones Web",
+      location: "IES Pedro Espinosa, Antequera, Málaga",
+      startDate: "2020",
+      endDate: "2022",
+      description: ["Aptitudes: HTML · CSS · GitHub · PHP · Java · JavaScript · SQL · Gestión de contenidos · Windows · Linux"],
+    },
+    {
+      name: "CFGM Sistemas Microinformáticos y Redes, Administración de Sistemas",
+      location: "IES Pedro Espinosa, Antequera, Málaga",
+      startDate: "2018",
+      endDate: "2020",
+      description: ["Aptitudes: Montaje de hardware informático · Sistemas Operativos · Cisco · Administración"],
+    },
+  ],
+
+  socialMedia: {
+    twitter: "https://x.com/CodesInfinity",
+    github: "https://github.com/CodesInfinity",
+    email: "agusmatic21gmail.com",
+    linkedin: "https://www.linkedin.com/in/agustin-rodriguez-aguilar/",
+  },
+
+  projects: [
+    {
+      title: "Agustin Rodriguez - Portfolio",
+      isFeatured: true,
+      thumbnail: "/assets/images/astro-portfolio.png",
+      githubUrl: "https://github.com/CodesInfinity/porfolio-astro/",
+      liveUrl: "https://github.com/CodesInfinity",
+    },
+  ],
+};
